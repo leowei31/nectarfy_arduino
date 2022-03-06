acb364c96df0f356e887e92dc186455f10ede680
Leo <leowei727@yahoo.ca>
2022-03-06 13:34:48 -0800

    thermocouple, dmt, and bluetooth ready to go

--- Nectarfy_arduino.ino
+++ Nectarfy_arduino.ino
@@ -1,8 +1,6 @@
 #include "max6675.h"
 #include <dht.h>
-
 dht DHT;
-
 #define DHT11_PIN 7
 
 int soPin = 4;
@@ -15,33 +13,49 @@ void setup() {
 // put your setup code here, to run once:
     Serial.begin(9600);
     Serial3.begin(9600);
+    pinMode(11,OUTPUT);
+    digitalWrite(11,HIGH);
+
 }
 
 void loop() {
 // put your main code here, to run repeatedly:
 
-//  int chk = DHT.read11(DHT11_PIN);
+int chk = DHT.read11(DHT11_PIN);
 
-//  Serial.print("Temperature From DHT: ");
-//  Serial.println(DHT.temperature);
-//  Serial.print("Humidity: ");
-//  Serial.println(DHT.humidity);
-  if (Serial3.available()){
-      Serial.println("Reading from bluetooth");
-      Serial.write(Serial3.read());
-  }
+double temp = DHT.temperature;
+double humid = DHT.humidity;
+
+Serial.print("Temperature From DHT: ");
+Serial.println(temp);
+Serial.print("Humidity: ");
+Serial.println(humid);
+float temp_from_Therm = Module.readCelsius();
+Serial.print("Temperature From Thermocouple: ");
+Serial.println(temp_from_Therm);
+
+digitalWrite(11,HIGH);
+
+Serial3.print("Temperature:");
+Serial3.println(temp);
+Serial3.print("Humidity:");
+Serial3.println(humid);
+
+
+delay(2000);
+
+if (Serial3.available()){
+    Serial.println("Reading from bluetooth");
+    char inByte = 0;
+    inByte = (char)Serial.read();
+    Serial.print((char)inByte);
+}
 //
-//  if (Serial.available()){
+//if (Serial.available()){
 //    Serial.println("Writing to bluetooth");
 //    Serial3.write(Serial.read());
-//  }  
-  float temp = Module.readCelsius();
-  Serial.print("Temperature From Thermocouple: ");
-  Serial.println(temp);
-  Serial3.print(temp);
+//} 
 
-  Serial.println();
-  delay(2000);
 
 
 }
