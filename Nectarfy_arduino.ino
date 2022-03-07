8c5dc6cf9cf84bfa6cec489329da13d33c034719
Leo <leowei727@yahoo.ca>
2022-03-06 23:01:53 -0800

    added control for relay

--- Nectarfy_arduino.ino
+++ Nectarfy_arduino.ino
@@ -7,14 +7,15 @@ int soPin = 4;
 int csPin = 5;
 int sckPin = 6;
 
+int pinOut = 3;
+
 MAX6675 Module(sckPin, csPin, soPin);
 
 void setup() {
 // put your setup code here, to run once:
     Serial.begin(9600);
     Serial3.begin(9600);
-    pinMode(11,OUTPUT);
-    digitalWrite(11,HIGH);
+    pinMode(3,OUTPUT);
 
 }
 
@@ -30,32 +31,34 @@ Serial.print("Temperature From DHT: ");
 Serial.println(temp);
 Serial.print("Humidity: ");
 Serial.println(humid);
+
 float temp_from_Therm = Module.readCelsius();
 Serial.print("Temperature From Thermocouple: ");
 Serial.println(temp_from_Therm);
-
-digitalWrite(11,HIGH);
 
 Serial3.print("Temperature:");
 Serial3.println(temp);
 Serial3.print("Humidity:");
 Serial3.println(humid);
 
+if (temp_from_Therm > 35){
+  digitalWrite(pinOut, LOW)
+} else {
+  digitalWrite(pinOut, HIGH);
+}
 
 delay(2000);
 
-if (Serial3.available()){
-    Serial.println("Reading from bluetooth");
-    char inByte = 0;
-    inByte = (char)Serial.read();
-    Serial.print((char)inByte);
-}
+//if (Serial3.available()){
+//    Serial.println("Reading from bluetooth");
+//    char inByte = 0;
+//    inByte = (char)Serial.read();
+//    Serial.print((char)inByte);
+//}
 //
 //if (Serial.available()){
 //    Serial.println("Writing to bluetooth");
 //    Serial3.write(Serial.read());
 //} 
-
-
 
 }
